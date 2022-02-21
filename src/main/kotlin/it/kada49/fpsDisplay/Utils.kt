0bb409504e0a936676126ec5b3a375e8f9138239
kada49 <seda18@rolmail.net>
2022-02-21 22:41:12 +0100

    removed redundant library

--- src/main/kotlin/it/kada49/fpsDisplay/Utils.kt
+++ src/main/kotlin/it/kada49/fpsDisplay/Utils.kt
@@ -5,10 +5,9 @@ import com.google.gson.JsonParser
 import gg.essential.universal.UChat
 import net.minecraft.client.Minecraft
 import net.minecraft.client.gui.ScaledResolution
-import okhttp3.OkHttpClient
-import okhttp3.Request
 import java.awt.Desktop
 import java.net.URI
+import java.net.URL
 
 
 object Utils {
@@ -17,12 +16,11 @@ object Utils {
         val gson = JsonParser()
         var result: String? = null
 
-        val client = OkHttpClient()
         try {
-            val request = Request.Builder().url(url).build()
-            val response = client.newCall(request).execute()
-            result = response.body?.string()
-        } catch (error: Error) { UChat.chat("An error occured requesting from '" + url + "': " + error.localizedMessage) }
+            result = URL(url).readText()
+        } catch (error: Error) {
+            UChat.chat("An error occurred requesting from '" + url + "': " + error.localizedMessage)
+        }
 
         return gson.parse(result) as JsonObject
     }
