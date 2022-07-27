754e57508f355c69250cda125039a58412227020
David <seda18@rolmail.net>
2022-07-27 22:42:51 +0200

    Optimized code, fixed typo

--- src/main/kotlin/it/kada49/fpsDisplay/FpsDisplayMod.kt
+++ src/main/kotlin/it/kada49/fpsDisplay/FpsDisplayMod.kt
@@ -7,21 +7,29 @@ import it.kada49.fpsDisplay.Constants.LOGGER
 import net.minecraftforge.client.ClientCommandHandler
 import net.minecraftforge.common.MinecraftForge
 import net.minecraftforge.fml.common.Mod
+import net.minecraftforge.fml.common.Mod.EventHandler
 import net.minecraftforge.fml.common.event.FMLInitializationEvent
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 import net.minecraftforge.fml.common.gameevent.TickEvent
 
 
-@Mod(name = Constants.NAME, modid = Constants.ID, version = Constants.VERSION, modLanguageAdapter = "gg.essential.api.utils.KotlinAdapter")
+@Mod(
+    name = Constants.NAME,
+    modid = Constants.ID,
+    version = Constants.VERSION,
+    modLanguageAdapter = "gg.essential.api.utils.KotlinAdapter",
+    updateJSON = "https://api.jsonstorage.net/v1/json/43187124-5250-4d1f-b4e7-65ea8e411651/8ab2e620-19a5-4f31-931e-7c9fc359fcf6",
+    guiFactory = "it.kada49.fpsDisplay.FpsDisplay.l"
+)
 
 object FpsDisplayMod{
 
     var vigilantGui: UScreen? = null
 
-    @Mod.EventHandler @Suppress("unused")
+    @EventHandler @Suppress("unused")
     fun init(event: FMLInitializationEvent){
 
-        LOGGER.info("Initialisation started for ${event.side}")
+        LOGGER.info("Initialization started for ${event.side}")
 
         Vigilance.initialize()
         Configuration.preload()
@@ -44,6 +52,4 @@ object FpsDisplayMod{
             vigilantGui = null
         }
     }
-
-
 }
