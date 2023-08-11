09176acb69131cefc0b523f683bb6dadd12cf9cb
David Senoner <david.senoner@student.tugraz.at>
2023-08-11 22:22:54 +0200

    Bump version

--- src/main/kotlin/it/kada49/Constants.kt
+++ src/main/kotlin/it/kada49/Constants.kt
@@ -4,7 +4,7 @@ package it.kada49
 object Constants {
     const val NAME = "FPS-Display"
     const val ID = "fps"
-    const val VERSION = "1.3.0"
+    const val VERSION = "1.3.1"
 
     const val PREFIX = "§5[§eFPS§5]"
 }
